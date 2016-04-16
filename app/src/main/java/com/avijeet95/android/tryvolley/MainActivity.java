136217e365ce0aac29bb1994f7ca4978cf156d59
Avijeet Gupta <avijeet95@ymail.com>
2016-04-16 14:12:43 +0530

    Image Loaded using Volley

--- app/src/main/java/com/avijeet95/android/tryvolley/MainActivity.java
+++ app/src/main/java/com/avijeet95/android/tryvolley/MainActivity.java
@@ -1,14 +1,14 @@
 package com.avijeet95.android.tryvolley;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.widget.ImageView;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,27 +19,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView = (TextView) findViewById(R.id.textView);
-
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        String url ="http://image.tmdb.org/t/p/original/inVq3FRqcYIRl2la8iZikYYxFNR.jpg";
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f68666c3fb6b4bc25006c4e421667376";
-
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+        // Retrieves an image specified by the URL, displays it in the UI.
+        ImageRequest request = new ImageRequest(url,
+                new Response.Listener<Bitmap>() {
                     @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        textView.setText("Response is: "+ response.substring(0,500));
+                    public void onResponse(Bitmap bitmap) {
+                        imageView.setImageBitmap(bitmap);
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
-            }
-        });
+                }, 0, 0, null,
+                new Response.ErrorListener() {
+                    public void onErrorResponse(VolleyError error) {
+                        imageView.setImageResource(R.mipmap.ic_launcher);
+                    }
+                });
 // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        queue.add(request);
     }
 }
